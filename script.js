3ee2b29ef5a46642d64a11183c39e7b957da19ca
Im3vulx <loic.hlppl@gmail.com>
2022-09-19 14:53:16 +0200

    création de slider

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ $("#menu-icon").on("click", function(){
 $("nav").slideToggle();
 $(this).toggleClass("active");
 });
+
+
+
+var mySwiper = new Swiper(".swiper-container", {
+    direction: "vertical",
+    loop: true,
+    pagination: ".swiper-pagination",
+    grabCursor: true,
+    speed: 1000,
+    paginationClickable: true,
+    parallax: true,
+    autoplay: false,
+    effect: "slide",
+    mousewheelControl: 1
+  });
+  
